790af708a939131fca7d4f0e4b81b2c0151b4de9
IvanChub <ichubp@miumg.edu.gt>
2020-10-06 00:08:03 -0600

    Primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
+
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ivan", country: "Guatemala"},
+    {id: 2, name: "Andrea", country: "USA"}
+  ];
+
+  selectEmployee: Employee = new Employee();
+
+
+  AgregarOrEditar()
+  {
+    if(this.selectEmployee.id === 0)
+    {
+      this.selectEmployee.id= this.employeeArray.length +1;
+      this.employeeArray.push(this.selectEmployee);
+    }
+    this.selectEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee)
+  {
+    this.selectEmployee = employee;
+  }
+
+  delete()
+  {
+    if(confirm('Desea eliminar al Empleado'))
+    {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectEmployee);
+      this.selectEmployee = new Employee();
+    }
+  }
 }
